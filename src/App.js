694497e93fba0fc12619411b997b0468614ca76a
ezeee21 <ezechielbatchi@gmail.com>
2025-01-07 16:09:30 +0100

    page restaurant et page chambre

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Accueil from './components/accueil/accueil';
+import Chambre from './components/chambre/chambre';
+import Resto from './components/resto/resto';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Header />}>
           <Route path='/' element={<Footer />}>
             <Route index element={<Accueil />} />
+            <Route path="/chambre" element={<Chambre />} />
+            <Route path="/resto" element={<Resto />} />
             <Route path="*" element={<h1>Page non trouvée</h1>} />
           </Route>
         </Route>
