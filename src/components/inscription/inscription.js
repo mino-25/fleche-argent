3d8ba2f5e7c5be0983db4e98dae91c1236e75fba
Solal Ribezzo <solal.ribezzo2003@gmail.com>
2025-01-12 16:30:31 +0100

    commentaire

--- src/components/inscription/inscription.js
+++ src/components/inscription/inscription.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './inscription.css';
 
 function Inscription() {
   const navigate = useNavigate();
